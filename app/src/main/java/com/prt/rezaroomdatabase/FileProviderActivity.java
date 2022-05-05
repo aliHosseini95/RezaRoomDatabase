0389fbbc9d40ad4ac70544d3025a00d4d33bfcba
aliHosseini95 <ali.h.dariush@gmail.com>
2022-05-05 17:16:47 +0430

    FileProvider

---
+++ app/src/main/java/com/prt/rezaroomdatabase/FileProviderActivity.java
@@ -0,0 +1,51 @@
+package com.prt.rezaroomdatabase;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FileProviderActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<String> fileResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.file_provider);
+        fileResultLauncher = registerForActivityResult(new ActivityResultContracts.GetContent()
+                , new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        if (result != null) {
+                            Bitmap bitmap = null;
+                            try {
+                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), result);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            if (bitmap != null) {
+                                ImageView imageView = findViewById(R.id.image_view);
+                                imageView.setImageBitmap(bitmap);
+                            }
+                        }
+                    }
+                });
+
+        findViewById(R.id.get_file_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fileResultLauncher.launch("image/*");
+            }
+        });
+    }
+}
